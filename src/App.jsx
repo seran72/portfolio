9555b4bfdb2b3cf8cff239d849ebe80518b8d83f
seran72 <sdiriba33@gmail.com>
2024-02-28 02:46:39 -0800

    Tech edit

--- src/App.jsx
+++ src/App.jsx
@@ -228,14 +228,14 @@
                 <div className="mt-8">
                   <div className="flex justify-between items-center">
                     <h2 className="font-semibold">Django</h2>
-                    <p className="text-gray-500">Advanced</p>
+                    <p className="text-gray-500">Intermediate</p>
                   </div>
                   <span className="w-[75%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
                 </div>
                 <div className="mt-8">
                   <div className="flex justify-between items-center">
                     <h2 className="font-semibold">React JS</h2>
-                    <p className="text-gray-500">Advanced</p>
+                    <p className="text-gray-500">Intermediate</p>
                   </div>
                   <span className="w-[50%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
                 </div>
